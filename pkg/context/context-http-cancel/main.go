b63286b1e14736e8cb1a45bb65b037a796ff4f5a
Érico Monteiro <erico.cintra@gmail.com>
2024-12-10 00:20:03 -0300

    cancel in http client

---
+++ pkg/context/context-http-cancel/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	response := make(map[string]string)
+	ctx := req.Context()
+	ctx, cancel := context.WithCancel(ctx)
+
+	defer cancel()
+
+	ch := make(chan string, 1)
+
+	go doSomething(ctx, ch)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("Context cancelled: ", ctx.Err())
+	case result := <-ch:
+		response["msg"] = result
+		responseJson, _ := json.Marshal(response)
+		w.Write(responseJson)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func doSomething(ctx context.Context, ch chan string) {
+	maxExecutions := 100
+	count := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			count++
+			time.Sleep(time.Millisecond * 100)
+			fmt.Printf("Execution %d\n", count)
+			if count >= maxExecutions {
+				ch <- "Hello, World!"
+			}
+		}
+	}
+}
