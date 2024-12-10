06013c9e0f64081eee7799d5219f745d59f023d3
Érico Monteiro <erico.cintra@gmail.com>
2024-12-09 23:15:38 -0300

    context with timout

--- pkg/context/context-timeout/main.go
+++ pkg/context/context-timeout/main.go
@@ -7,15 +7,26 @@ import (
 )
 
 func main() {
-	// Channel used to receive the result from doSomething function
-	ch := make(chan string, 1)
+	var ctxTimeout context.Context
+	var ch chan string
+	var cancel context.CancelFunc
 
-	// Create a context with a timeout of 5 seconds
-	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
+	defer func() {
+		if cancel != nil {
+			cancel()
+		}
+	}()
+
+	// Expect timeout ----
+	fmt.Println("=== Expect timeout ===")
+	// Channel used to receive the result from doSomething function
+	ch = make(chan string, 1)
+
+	// Create a context with a timeout of 3 seconds
+	ctxTimeout, cancel = context.WithTimeout(context.Background(), time.Second*3)
 
 	// Start the doSomething function
-	go doSomething(ctxTimeout, ch)
+	go doSomething(ctxTimeout, "case 01", 4, ch)
 
 	// Wait for one of the following to happen:
 	select {
@@ -24,11 +35,59 @@ func main() {
 	case result := <-ch:
 		fmt.Printf("Received: %s\n", result)
 	}
+	fmt.Println("\n")
+
+	// Expect success with timeout ----
+	fmt.Println("=== Expect execute with success (cancelable context) ===")
+	// Channel used to receive the result from doSomething function
+	ch = make(chan string, 1)
+
+	// Create a context with a timeout of 5 seconds but uncancelable
+	ctxTimeout, cancel = context.WithTimeout(context.Background(), time.Second*3)
+	ctxTimeout = context.WithoutCancel(ctxTimeout)
+
+	// Start the doSomething function
+	go doSomething(ctxTimeout, "case 02", 4, ch)
+
+	// Wait for one of the following to happen:
+	select {
+	case <-ctxTimeout.Done():
+		fmt.Printf("Context cancelled: %v\n", ctxTimeout.Err())
+	case result := <-ch:
+		fmt.Printf("Received: %s\n", result)
+	}
+	fmt.Println("\n")
+
+	// Expect success with timeout ----
+	fmt.Println("=== Expect execute with success ===")
+	// Channel used to receive the result from doSomething function
+	ch = make(chan string, 1)
+
+	// Create a context with a timeout of 5
+	ctxTimeout, cancel = context.WithTimeout(context.Background(), time.Second*5)
+	ctxTimeout = context.WithoutCancel(ctxTimeout)
+
+	// Start the doSomething function
+	go doSomething(ctxTimeout, "case 03", 4, ch)
+
+	// Wait for one of the following to happen:
+	select {
+	case <-ctxTimeout.Done():
+		fmt.Printf("Context cancelled: %v\n", ctxTimeout.Err())
+	case result := <-ch:
+		fmt.Printf("Received: %s\n", result)
+	}
+	fmt.Println("\n")
 }
 
-func doSomething(ctx context.Context, ch chan string) {
-	fmt.Println("doSomething Sleeping...")
-	time.Sleep(time.Second * 2)
-	fmt.Println("doSomething Wake up...")
+func expectTimeout(ctx context.Context) {
+
+}
+
+func doSomething(ctx context.Context, prefix string, secondsToWait int, ch chan string) {
+	duration := time.Duration(secondsToWait) * time.Second
+	fmt.Println(prefix + " doSomething Sleeping...")
+	time.Sleep(duration)
+	fmt.Println(prefix + " doSomething Wake up...")
 	ch <- "Did Something"
 }
